6a0432917117c455ac2d8415451d62e36af1311a
pjs <pjseoane@gmail.com>
2019-07-17 23:59:36 -0300

    1st Commit paulinux

---
+++ RestAccess-paulinux/src/main/java/com/qaant/rofexmaven/MainAccess.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qaant.rofexmaven;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * * 
+ * @author pjseoane@qaantcap.com
+ */
+public class MainAccess {
+    
+    public static String marketID;
+    public static String endpoint;
+    public static String usr;
+    public static String pswd;
+    public static String ticker;
+    public static String author;
+    
+    public static void main(String[] args) throws Exception{
+          
+        
+    try (InputStream input = new FileInputStream ("src/main/java/com/qaant/configuration/config.properties")){
+    
+        Properties prop =new Properties();
+        prop.load(input);
+            
+        // *** upload variables in cfg file
+            
+        marketID        =prop.getProperty("ra.marketID");
+        endpoint        =prop.getProperty("ra.endpoint");
+        usr             =prop.getProperty("ra.usr");
+        pswd            =prop.getProperty("ra.pswd");
+        ticker          =prop.getProperty("ra.exampleTicker");
+        author          =prop.getProperty("ra.author");
+            
+             
+        // loginREST newConnection = new loginREST(); 
+        System.out.println("Author :"+author);
+            
+        
+        // print outputs
+        System.out.println("\nREST Outputs:....");
+        System.out.println("\nTOKEN->>>>>>>>>>:"+loginREST.getToken());
+            
+        String instruments =loginREST.getInstruments();
+        System.out.println("Elements "+instruments);
+           
+        String instDetails =loginREST.getInstrumentDetail(ticker);
+        System.out.println("Detail "+instDetails);
+           
+             
+    }catch (IOException ex){
+        System.out.println("Some error loading config.properties");
+    }
+        
+    }
+}
---
+++ RestAccess-paulinux/src/main/java/com/qaant/rofexmaven/loginREST.java
@@ -0,0 +1,86 @@
+/*
+ 
+ */
+package com.qaant.rofexmaven;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+
+
+/**
+ *
+ * @author pjseoane@qaantcap.com
+ */
+public final class loginREST {
+    
+    public static String marketID;
+    public static String endpoint;
+    public static String usr;
+    public static String pswd;
+    public static String ticker;
+    
+    
+    private static String url;
+    private static String token=null;
+          
+    
+    loginREST() throws Exception{
+        //System.out.println(msg);
+        System.out.println("Login to: "+MainAccess.endpoint);
+        System.out.println("Processing token...:");
+        getToken();
+    }        
+
+    public static String getToken()throws Exception{
+        if (token == null){
+            url=    MainAccess.endpoint+ "auth/getToken";
+        
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            String userCredentials = MainAccess.usr+":"+MainAccess.pswd;
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+        
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Authorization", basicAuth);
+            con.setDoOutput(true);
+            token=con.getHeaderField("X-Auth-Token");
+        }
+           
+        return token;
+    }
+    
+    private static String doGET(String url) throws Exception{
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestProperty("X-Auth-Token",token);
+        con.setRequestMethod("GET");
+               
+        BufferedReader in;
+        in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        
+        String inputline;
+        StringBuilder content = new StringBuilder();
+                
+        while ((inputline = in.readLine()) != null) {
+            content.append(inputline);
+        }
+        in.close();
+        con.disconnect();
+        return content.toString();
+        
+    }
+    public static String getInstruments()throws Exception{
+        url = MainAccess.endpoint+ "rest/instruments/all";
+        
+        return doGET(url);
+    }
+    public static String getInstrumentDetail(String symbol)throws Exception{
+        url = MainAccess.endpoint + "rest/instruments/detail?symbol=" + symbol + "&marketId=" + MainAccess.marketID;
+        return doGET(url);
+    }
+}
+
